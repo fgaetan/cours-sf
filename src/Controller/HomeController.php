8aca40a986340d678c941de2454d0cbbb3b4a136
fgaetan <frion.gaetan@gmail.com>
2022-05-04 10:10:20 +0200

    04 05 2022 di +factorisation

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    private $repoArticle;
+
+    public function __construct(ArticleRepository $repoArticle)
+    {
+        $this->repoArticle = $repoArticle;
+    }
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        $articles = $this->repoArticle->findAll();
+        return $this->render('home/index.html.twig', [
+            "articles" => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     */
+    public function show(Article $article): Response
+    {
+        if (!$article) {
+            $this->redirectToRoute("home");
+        }
+        return $this->render(' home/show.html.twig', [
+            "article" => $article
+        ]);
+    }
+}
